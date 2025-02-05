567e1c365177d5b0de878683e9e24fd1ac66e3df
TawanTippkun200501 <tawantippkun48@gmail.com>
2025-02-05 22:43:49 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('Hello World!');
+document.write("<h1>Team and cartoon levo</h1>");
+
+
+console.log("team levo cartoon");
+console.error("your name");
+console.warn("กรุณากรอบชื่อผู้ใช้อีกครั้ง");
