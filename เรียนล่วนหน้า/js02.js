f2f653510e860861f741932e1aa7c0d1559a2581
TawanTippkun200501 <tawantippkun48@gmail.com>
2025-02-07 18:21:01 +0700

    update

---
+++ เรียนล่วนหน้า/js02.js
@@ -0,0 +1,31 @@
+//if else
+
+if(5==5){
+  console.log("Hello");
+}
+
+let i = 3434;
+let o = 23990;
+if(i>0){
+  console.log("เงินเข้า :",i+o)
+}
+else if(o>0){
+  console.log("เงินออก :",i-o)
+}
+else{
+  console.error("เงินไม่มี")
+}
+
+let $ = 230666;
+let p = 10;
+if($>=p){
+  if($<p){
+    console.log("เงินเพิ่มขึ้น :",$+p);
+  }
+  else if($<p){
+    console.log("เงินเพิ่มขึ้น :",$+p)
+  }
+  else{
+    console.log("เงินลดลง :",$-p)
+  }
+}
