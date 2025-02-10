acf7c03efe40d8a5b804e49cf808b6a497527b14
TawanTipppkun200501 <tawantippkun48@gmail.com>
2025-02-10 06:29:36 +0000

    js

--- สรุป/JAVASCRIPT2.js
+++ สรุป/JAVASCRIPT2.js
@@ -8,3 +8,12 @@
   กหดหกดหก
   dfsdfds
 */
+
+// document.write("team love cartoon")
+let i = "245524";
+a = +i;
+console.log(a, typeof a);
+
+let o = 4242342;
+y = ""+(o);
+console.log(y, typeof y);
--- เขียนฝึก js/c.js
+++
@@ -1,3 +0,0 @@
-document.write("<h1>Hello</h1>");
-console.log("hello");
-console.error("hello")
